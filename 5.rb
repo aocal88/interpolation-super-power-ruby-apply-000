44b4787c268c34716654e09755a20c18dd61ab60
HAN O <a.ocal88@gmail.com>
2020-06-22 21:28:40 +0000

    Automatically backed up by Learn

---
+++ 5.rb
@@ -0,0 +1,9 @@
+name = "Heeyoung"
+
+puts name
+
+puts"1+1"
+
+puts "#{1+1}"
+
+puts "Hello #{name}. How are you?"
---
+++ lib/new.rb
@@ -0,0 +1,8 @@
+
+sarina = ["Sarina", 30, "model"]
+
+puts "name: #{sarina[0]}"
+puts "age: #{sarina[1]}"
+puts "occupation: #{sarina[2]}"
+
+puts "name: #{sarina[0]}, age: #{sarina[1]}, occupation: #{sarina[2]} "
